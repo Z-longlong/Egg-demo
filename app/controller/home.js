ab78c11b16a7413f275c2817b9f60b4b8db1d962
龙龙 <Gent_zhouyulong@163.com>
2019-12-03 15:13:41 +0800

    新增模板渲染

--- app/controller/home.js
+++ app/controller/home.js
@@ -8,6 +8,13 @@ class HomeController extends Controller {
         const userInfo = await ctx.service.home.find();
         ctx.body = userInfo;
     }
+    async getHome() {
+        const { ctx } = this;
+        const title = '龙龙的Egg-Demo';
+        await ctx.render('index', {
+            title,
+        });
+    }
 }
 
 module.exports = HomeController;
--- config/config.default.js
+++ config/config.default.js
@@ -5,26 +5,41 @@
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
+
+const path = require('path');
+
 module.exports = appInfo => {
-  /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
-   **/
-  const config = exports = {};
+    /**
+     * built-in config
+     * @type {Egg.EggAppConfig}
+     **/
+    const config = exports = {};
 
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1575256308066_2229';
+    // use for cookie sign key, should change to your own and keep security
+    config.keys = appInfo.name + '_1575256308066_2229';
 
-  // add your middleware config here
-  config.middleware = [];
+    // add view template
+    config.view = {
+        root: [
+            path.join(appInfo.baseDir, 'app/view'),
+        ].join(','),
+        defaultViewEngine: 'nunjucks',
+        // 左边写成.html后缀，会自动渲染.html文件
+        mapping: {
+            '.html': 'nunjucks',
+        },
+    };
 
-  // add your user config here
-  const userConfig = {
-    // myAppName: 'egg',
-  };
+    // add your middleware config here
+    config.middleware = [];
 
-  return {
-    ...config,
-    ...userConfig,
-  };
+    // add your user config here
+    const userConfig = {
+        // myAppName: 'egg',
+    };
+
+    return {
+        ...config,
+        ...userConfig,
+    };
 };
